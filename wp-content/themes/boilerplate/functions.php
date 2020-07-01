dc148149866edf52471b0f863fb7e9a2d3694f95
Hiroshi Urabe <urabe@ham.works>
2020-07-01 18:13:55 +0900

    fix css

--- wp-content/themes/boilerplate/functions.php
+++ wp-content/themes/boilerplate/functions.php
@@ -7,46 +7,53 @@
  * @package boilerplate
  */
 
-if ( ! function_exists( 'boilerplate_setup' ) ) :
 
-	function boilerplate_setup() {
+function boilerplate_setup() {
 
-		load_theme_textdomain( 'boilerplate', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'boilerplate', get_template_directory() . '/languages' );
 
-		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'boilerplate' ),
-		) );
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'menu-1' => esc_html__( 'Primary', 'boilerplate' ),
+	) );
 
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
-endif;
+	/**
+	 * Add support for core custom logo.
+	 *
+	 * @link https://codex.wordpress.org/Theme_Logo
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 250,
+		'width'       => 250,
+		'flex-width'  => true,
+		'flex-height' => true,
+	) );
+}
+
 add_action( 'after_setup_theme', 'boilerplate_setup' );
+
+function boilerplate_editor_styles() {
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'build/style-index.css' );
+}
+
+add_action( 'after_setup_theme', 'boilerplate_editor_styles' );
+
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -58,19 +65,27 @@ add_action( 'after_setup_theme', 'boilerplate_setup' );
 function boilerplate_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'boilerplate_content_width', 640 );
 }
+
 add_action( 'after_setup_theme', 'boilerplate_content_width', 0 );
 
 /**
  * Enqueue scripts and styles.
  */
 function boilerplate_scripts() {
-	wp_enqueue_style( 'gutenbergbase-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'boilerplateblocks-style', get_template_directory_uri() . '/css/blocks.css' );
+	wp_enqueue_style( 'boilerplate-style', get_template_directory_uri() . '/build/style-index.css' );
 
-	wp_enqueue_script( 'boilerplate-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'boilerplate-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	if ( file_exists( dirname( __FILE__ ) . '/build/index.asset.php' ) ) {
+		$asset_file = include( dirname( __FILE__ ) . '/build/index.asset.php' );
+		wp_enqueue_script(
+			'boilerplate-script',
+			get_template_directory_uri() . '/build/index.js',
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true
+		);
+	}
 }
+
 add_action( 'wp_enqueue_scripts', 'boilerplate_scripts' );
 
 /**
