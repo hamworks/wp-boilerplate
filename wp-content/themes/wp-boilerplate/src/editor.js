95ac6307e678e08159e11e628a7dacdbaab71241
Chiaki Obara <obara@ham.works>
2021-01-05 11:35:23 +0900

    cssファイル整理

--- wp-content/themes/wp-boilerplate/src/editor.js
+++ wp-content/themes/wp-boilerplate/src/editor.js
@@ -1,11 +1,6 @@
 import './css/_variables.css';
-
-import 'normalize.css';
 import './css/_elements.css';
 import './css/_accessibility.css';
 import './css/_alignment.css';
 
 import './css/blocks/_blocks.css';
-
-import './js/navigation';
-import './js/skip-link-focus-fix';
